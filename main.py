435342bdccc72f0efdc8fc67fb3bba59b51cb9bd
美波 <anatawahitoka@gmail.com>
2025-07-10 22:11:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -167,62 +167,66 @@ async def youtube_feed_check_loop():
         await bot.wait_until_ready()
 
         channel = bot.get_channel(1328406450489393253)
-        feed_url = 'https://www.youtube.com/feeds/videos.xml?channel_id=UC5abJGhz74y-cw88wFqX0Jw'
+        feed_urls = [
+            'https://www.youtube.com/feeds/videos.xml?channel_id=UC5abJGhz74y-cw88wFqX0Jw',
+            'https://www.youtube.com/feeds/videos.xml?channel_id=UCoLxgTtHYNA8AjOJB1rU-2g'
+        ]
 
         while not bot.is_closed():
             try:
-                feed = feedparser.parse(feed_url)
+                for feed_url in feed_urls:
+                    feed = feedparser.parse(feed_url)
 
-                if feed.entries:
-                    latest_entry = feed.entries[0]
-                    try:
-                        video_id = latest_entry.yt_videoid
-                    except AttributeError:
-                        if "youtube.com/watch?v=" in latest_entry.link:
-                            video_id = latest_entry.link.split("v=")[1].split("&")[0]
-                        elif "youtu.be/" in latest_entry.link:
-                            video_id = latest_entry.link.split("youtu.be/")[1].split("?")[0]
-                        else:
-                            video_id = None
+                    if feed.entries:
+                        latest_entry = feed.entries[0]
+                        try:
+                            video_id = latest_entry.yt_videoid
+                        except AttributeError:
+                            if "youtube.com/watch?v=" in latest_entry.link:
+                                video_id = latest_entry.link.split("v=")[1].split("&")[0]
+                            elif "youtu.be/" in latest_entry.link:
+                                video_id = latest_entry.link.split("youtu.be/")[1].split("?")[0]
+                            else:
+                                video_id = None
 
-                    video_title = latest_entry.title
-                    video_url = f"https://www.youtube.com/watch?v={video_id}"
+                        video_title = latest_entry.title
+                        video_url = f"https://www.youtube.com/watch?v={video_id}"
 
-                    if latest_video_id != video_id:
-                        latest_video_id = video_id
-                        save_latest_video_id(video_id)
+                        if latest_video_id != video_id:
+                            latest_video_id = video_id
+                            save_latest_video_id(video_id)
 
-                        if channel:
+                            if channel:
 
-                            video_already_sent = False
-                            try:
-                                async for old_message in channel.history(limit=50):
-                                    if video_url in old_message.content:
-                                        video_already_sent = True
-                                        break
-                            except Exception as e:
-                                pass
+                                video_already_sent = False
+                                try:
+                                    async for old_message in channel.history(limit=50):
+                                        if video_url in old_message.content:
+                                            video_already_sent = True
+                                            break
+                                except Exception as e:
+                                    pass
 
-                            if not video_already_sent:
-                                thumbnail_url = f"https://img.youtube.com/vi/{video_id}/maxresdefault.jpg"
-                                video_description = getattr(latest_entry, 'summary', 'No description available')
-                                video_content = getattr(latest_entry, 'content', [{}])
-                                if video_content and len(video_content) > 0:
-                                    content_value = video_content[0].get('value', 'No content available')
-                                else:
-                                    content_value = 'No content available'
+                                if not video_already_sent:
+                                    thumbnail_url = f"https://img.youtube.com/vi/{video_id}/maxresdefault.jpg"
+                                    video_description = getattr(latest_entry, 'summary', 'No description available')
+                                    video_content = getattr(latest_entry, 'content', [{}])
+                                    if video_content and len(video_content) > 0:
+                                        content_value = video_content[0].get('value', 'No content available')
+                                    else:
+                                        content_value = 'No content available'
 
-                                embed = discord.Embed(
-                                    title=video_title,
-                                    url=video_url,
-                                    description=video_description,
-                                    color=0xFFA500
-                                )
-                                embed.set_image(url=thumbnail_url)
-                                embed.add_field(name="Video URL", value=video_url, inline=False)
-                                embed.add_field(name="Thumbnail URL", value=thumbnail_url, inline=False)
+                                    embed = discord.Embed(
+                                        title=video_title,
+                                        url=video_url,
+                                        description=video_description,
+                                        color=0xFFA500
+                                    )
+                                    embed.set_image(url=thumbnail_url)
+                                    embed.add_field(name="Video URL", value=video_url, inline=False)
+                                    embed.add_field(name="Thumbnail URL", value=thumbnail_url, inline=False)
 
-                                await channel.send(content="<@&1383766143939903528>", embed=embed)
+                                    await channel.send(content="<@&1383766143939903528>", embed=embed)
 
             except Exception as e:
                 pass
@@ -461,26 +465,39 @@ async def terms(ctx):
 @bot.command()
 async def showcase(ctx):
             try:
-                feed_url = 'https://www.youtube.com/feeds/videos.xml?channel_id=UC5abJGhz74y-cw88wFqX0Jw'
-                feed = feedparser.parse(feed_url)
+                feed_urls = [
+                    'https://www.youtube.com/feeds/videos.xml?channel_id=UC5abJGhz74y-cw88wFqX0Jw',
+                    'https://www.youtube.com/feeds/videos.xml?channel_id=UCoLxgTtHYNA8AjOJB1rU-2g'
+                ]
+                
+                latest_video = None
+                latest_published = None
+                
+                for feed_url in feed_urls:
+                    feed = feedparser.parse(feed_url)
+                    if feed.entries:
+                        entry = feed.entries[0]
+                        published = entry.published_parsed
+                        if latest_published is None or published > latest_published:
+                            latest_video = entry
+                            latest_published = published
 
-                if feed.entries:
-                    latest_entry = feed.entries[0]
+                if latest_video:
                     try:
-                        video_id = latest_entry.yt_videoid
+                        video_id = latest_video.yt_videoid
                     except AttributeError:
-                        if "youtube.com/watch?v=" in latest_entry.link:
-                            video_id = latest_entry.link.split("v=")[1].split("&")[0]
-                        elif "youtu.be/" in latest_entry.link:
-                            video_id = latest_entry.link.split("youtu.be/")[1].split("?")[0]
+                        if "youtube.com/watch?v=" in latest_video.link:
+                            video_id = latest_video.link.split("v=")[1].split("&")[0]
+                        elif "youtu.be/" in latest_video.link:
+                            video_id = latest_video.link.split("youtu.be/")[1].split("?")[0]
                         else:
                             video_id = None
 
-                    video_title = latest_entry.title
+                    video_title = latest_video.title
                     video_url = f"https://www.youtube.com/watch?v={video_id}"
                     thumbnail_url = f"https://img.youtube.com/vi/{video_id}/maxresdefault.jpg"
-                    video_description = getattr(latest_entry, 'summary', 'No description available')
-                    video_content = getattr(latest_entry, 'content', [{}])
+                    video_description = getattr(latest_video, 'summary', 'No description available')
+                    video_content = getattr(latest_video, 'content', [{}])
                     if video_content and len(video_content) > 0:
                         content_value = video_content[0].get('value', 'No content available')
                     else:
@@ -498,9 +515,9 @@ async def showcase(ctx):
 
                     await ctx.send(content="<@&1383766143939903528>", embed=embed)
                 else:
-                    await ctx.send("No videos found in the YouTube feed.")
+                    await ctx.send("No videos found in the YouTube feeds.")
             except Exception as e:
-                await ctx.send(f"Error fetching YouTube feed: {str(e)}")
+                await ctx.send(f"Error fetching YouTube feeds: {str(e)}")
 
 @bot.command()
 async def loginsignup(ctx):
